a8f62d94819d126db955ba062bfd4f5827d41da1
thakursaajid-design <thakursaajid@gmail.com>
2026-09-03 11:14:19 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// Google Apps Script web app URL
+window.CHECKLIST_CONFIG = {
+  googleScriptUrl: "https://script.google.com/macros/s/AKfycby0t811kvzxflOYesq5GcRL54X1yF0epRf6_YoBdytUb8Fue0SKrsboBeAlvckOzWU/exec"
+};
