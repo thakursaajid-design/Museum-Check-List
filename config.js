df721e10cfda147e3b518275ee8099de8926bf36
thakursaajid-design <thakursaajid@gmail.com>
2026-09-03 11:13:40 +0300

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// Google Apps Script web app URL
-window.CHECKLIST_CONFIG = {
-  googleScriptUrl: "https://script.google.com/macros/s/AKfycby0t811kvzxflOYesq5GcRL54X1yF0epRf6_YoBdytUb8Fue0SKrsboBeAlvckOzWU/exec"
-};
